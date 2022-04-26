c9cde8fbea9ad3f8d27dd77f4f1ae528e1e5434c
Oskar Sęk <oskar.sek@itpendent.com>
2022-04-26 18:50:51 +0200

    New functionality addition

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -3,10 +3,15 @@ package com.calculator;
 final public class Calculator {
 
     public static void main(String[] args) {
+        System.out.println(addition(12, 54));
         System.out.println(Calculator.subtract(5, 10));
     }
 
     public static double subtract(double a, double b) {
         return a - b;
+    }
+
+    public static double addition(double a, double b) {
+        return a + b;
     }
 }
